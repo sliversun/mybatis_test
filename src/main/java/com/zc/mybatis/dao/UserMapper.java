64f423304498f218a5ade9db60e2303c4f9d7041
sliversun <475895422@qq.com>
2017-09-19 17:45:54 +0800

    补全User注解增删改查

--- src/main/java/com/zc/mybatis/dao/UserMapper.java
+++ src/main/java/com/zc/mybatis/dao/UserMapper.java
@@ -1,16 +1,22 @@
 package com.zc.mybatis.dao;
 
 import com.zc.mybatis.entity.User;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 /*
 *
-* Mybatis注解测试   =====  有很多弊端，可扩展性差，无法完成 一对多 的映射
+* Mybatis注解测试   =====  有很多弊端:1.可扩展性差，无法完成 一对多 的映射
+*                                      2.不支持mybatis动态语言书写
 * */
 @Mapper
 public interface UserMapper {
     @Select("select * from m_user")
      List<User>  findUser();
+    @Update(" update m_user set name = #{name}, age = #{age} where id = #{id}")
+     void update（User user）;
+    @Insert("insert into  m_user(id,name,age)  values(#{id},#{name},#{age})")
+     void add(User user);
+    @Delete("delete from m_user WHERE id = #{id}")
+     void delete(int  id);
 }
